dc65aae37c243d97580803da5063af1ac15cd6bc
fifanuurunhalizah09 <eljeaje@gmail.com>
2024-11-07 09:59:20 +0700

    komit

--- Bioskop09.java
+++ Bioskop09.java
@@ -16,11 +16,17 @@ public class Bioskop09 {
     System.out.printf("%s \t %s \n", penonton[2][0], penonton[2][1]);
     System.out.printf("%s \t %s \n", penonton[3][0], penonton[3][1]);
 
+    System.out.println(penonton.length);
+
+    for (String[] barisPenonton : penonton) {
+        System.out.println("Panjang baris: " + barisPenonton.length);
+    }
+    System.out.println("Penonton pada baris ke-3: ");
 
     for (int i = 0; i < penonton.length; i++) {
         System.out.println("Penonton pada baris ke- " + (i+1) + ": " + String.join(", ", penonton[i]));
     }
-    
 
+    
     } 
 }
